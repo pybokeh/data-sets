017b307c7a418ff2ded6bca0442d8e042a3f2477
pybokeh <pybokeh@protonmail.com>
2026-08-23 17:40:39 -0400

    Corrected start_postgres.bat file

--- postgres/start_postgres.bat
+++ postgres/start_postgres.bat
@@ -1,11 +1,11 @@
-REM You want to remove "REM" in the line containing the initdb command.  Run this .bat file once.  Then add REM back, then run the .bat file again.
+REM Only run the initdb command once, then comment the line - place REM in front
 @ECHO ON
-@SET PATH="%~dp0\bin";%PATH%
-@SET PGDATA=%~dp0\data
+@SET "PATH=%~dp0bin;%PATH%"
+@SET PGDATA=%~dp0data
 @SET PGDATABASE=postgres
 @SET PGUSER=postgres
 @SET PGPORT=5432
-@SET PGLOCALEDIR=%~dp0\share\locale
+@SET PGLOCALEDIR=%~dp0share\locale
 REM initdb -U postgres -W -E UTF8 -D "%PGDATA%" --auth=trust
-"%~dp0\bin\pg_ctl" -D "%PGDATA%" -o "-p %PGPORT%" -l logfile start
+"%~dp0bin\pg_ctl" -D "%PGDATA%" -o "-p %PGPORT%" -l logfile start
 pause
